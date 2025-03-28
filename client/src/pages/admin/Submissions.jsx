1fe269f6675969a38bb5587b48d6bcad6a379af1
Vijayeswar-Yalamanchili <vijayeswar_y@yahoo.com>
2025-03-29 05:19:11 +0530

    updated

--- client/src/pages/admin/Submissions.jsx
+++ client/src/pages/admin/Submissions.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { toast } from 'react-toastify'
-import { setAssignments } from '../../redux/adminDashboardSlice'
 import ApiRoutes from '../../utils/ApiRoutes'
 import AxiosService from '../../utils/AxiosService'
 import { useDispatch } from 'react-redux'
@@ -19,16 +18,11 @@ function Submissions() {
 
   const handleGetCurrentAssignment = async() => {
     try {
-      // setLoading(true)
       let res = await AxiosService.get(`${ApiRoutes.ADMINCURRENTASSIGNMENT.path}/${params.assignmentId}/${userId}`, {headers : { 'Authorization' : `${getLoginToken}` }})
-      console.log(res.data)
       if(res.status === 200){
         setassignmentDetails(res.data.currentassignment)
-        // setassignmentNameData(res.data.currentassignment.name)
       }
-      // setLoading(false)
     } catch (error) {
-      console.log(error)
       toast.error(error.response.data.message || error.message)        
     }
   }
@@ -38,7 +32,20 @@ function Submissions() {
   }, [])
 
   return <>
-    
+    <div className="p-4">
+      <h2 className="text-lg font-semibold">{assignmentDetails.name} - Submitted By:</h2>
+      {
+        assignmentDetails.taskSubmittedBy ? (
+          <ol className="list-decimal ml-6">
+            {assignmentDetails.taskSubmittedBy.map((id, index) => (
+              <li key={index} className="text-gray-800">{id}</li>
+            ))}
+          </ol>
+        ) : (
+          <p>Loading...</p>
+        )
+      }
+    </div>
   </>
 }
 
